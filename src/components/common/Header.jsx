f6bcdc8b719f5b3f8fb5956692531d77820fcfde
Austin-Choi <austinsupra@gmail.com>
2024-11-26 22:38:08 +0900

    feat : 모바일 상태창 고려해서 spacing 적용

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 import './Header.css';
 
-const Header = ({ showMyPageButton = false }) => {
+const Header = () => {
+  const location = useLocation();
+
+  // Onboarding 페이지에서는 마이페이지 버튼 숨기기
+  const showMyPageButton = location.pathname !== '/';
+
   return (
     <header className="header">
-      <div className="header-spacer"></div>
       <div className="header-logo">LOGO</div>
       {showMyPageButton && (
         <button
           className="header-button"
           onClick={() => alert('Go to My Page')}
         >
-          My Page
+          My
         </button>
       )}
     </header>

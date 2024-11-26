ef5a78efd02104253232d32d230efa69f4208558
Austin-Choi <austinsupra@gmail.com>
2024-11-26 22:38:42 +0900

    patch : MainLayout에서 children으로 각 screen 로딩하는 방식으로 전환

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import MainLayout from './layouts/MainLayout';
 import OnboardingScreen from './components/OnboardingScreen';
 import CallHomeScreen from './components/CallHomeScreen';
 import CallScreen from './components/CallScreen';
@@ -9,11 +10,13 @@ const App = () => {
   return (
     <SocketProvider>
       <Router>
-        <Routes>
-          <Route path="/" element={<OnboardingScreen />} />
-          <Route path="/call/home" element={<CallHomeScreen />} />
-          <Route path="/call/:roomName" element={<CallScreen />} />
-        </Routes>
+        <MainLayout>
+          <Routes>
+            <Route path="/" element={<OnboardingScreen />} />
+            <Route path="/call/home" element={<CallHomeScreen />} />
+            <Route path="/call/:roomName" element={<CallScreen />} />
+          </Routes>
+        </MainLayout>
       </Router>
     </SocketProvider>
   );

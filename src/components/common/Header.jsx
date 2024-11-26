6d434fffaf426ccc98a23768ea3b45c77d2f99d4
Austin-Choi <austinsupra@gmail.com>
2024-11-26 22:58:47 +0900

    feat : responsive Logo 컴포넌트 구현

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,22 +1,21 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
 import './Header.css';
+import Logo from './Logo';
 
 const Header = () => {
   const location = useLocation();
-
-  // Onboarding 페이지에서는 마이페이지 버튼 숨기기
   const showMyPageButton = location.pathname !== '/';
 
   return (
     <header className="header">
-      <div className="header-logo">LOGO</div>
+      <Logo />
       {showMyPageButton && (
         <button
           className="header-button"
           onClick={() => alert('Go to My Page')}
         >
-          My
+          My Page
         </button>
       )}
     </header>

8e99f78c0dcd29648b323733dddddddc7f68bfbe
Austin-Choi <austinsupra@gmail.com>
2024-11-29 18:15:55 +0900

    patch : 채팅창 닉네임 없을때는 닉네임으로 표현

--- src/components/CallChatScreen.jsx
+++ src/components/CallChatScreen.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import CallControl from './CallControl';
 import ChatBox from './ChatBox';
 import './CallChatScreen.css';
 
 const CallChatScreen = ({
+  nickname,
   onEndCall,
   messages,
   onSendMessage,
@@ -11,17 +13,7 @@ const CallChatScreen = ({
 }) => {
   return (
     <div className="call-chat-screen">
-      <div className="call-control">
-        <img
-          src="/path/to/default-profile.png"
-          alt="Profile"
-          className="profile-image"
-        />
-        <p className="nickname">닉네임</p>
-        <button className="end-call-button" onClick={onEndCall}>
-          통화 종료
-        </button>
-      </div>
+      <CallControl nickname={nickname} onEndCall={onEndCall} />
       <ChatBox
         messages={messages}
         onSendMessage={onSendMessage}

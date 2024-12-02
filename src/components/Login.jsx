3a20a661a942dab8465664005d6df2c6ba56f0b1
Austin-Choi <austinsupra@gmail.com>
2024-12-02 21:36:47 +0900

    patch : 리다이렉션 이후 액세스 토큰 저장

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,7 +4,7 @@ import GoogleIcon from '/google-icon.svg';
 import KakaoIcon from '/kakao-icon.svg';
 
 const Login = () => {
-  const handleSocialLogin = (provider) => {
+  const handleSocialLogin = provider => {
     const loginUrl = `${import.meta.env.VITE_API_BASE_URL}/oauth2/authorization/${provider}`;
 
     console.log(`Redirecting to ${provider} login:`, loginUrl);
@@ -13,18 +13,33 @@ const Login = () => {
     window.location.href = loginUrl;
   };
 
+  // 리디렉션 후 Access Token 처리
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const accessToken = params.get('accessToken');
 
-  
+    if (accessToken) {
+      localStorage.setItem('Authorization', `Bearer ${accessToken}`);
+      console.log('Access Token 저장:', accessToken);
+    }
+  }, []);
+
   return (
     <div className="login-container">
       {/* 구글 로그인 버튼 */}
-      <button className="login-button google-login" onClick={() => handleSocialLogin('google')}>
+      <button
+        className="login-button google-login"
+        onClick={() => handleSocialLogin('google')}
+      >
         <img src={GoogleIcon} alt="Google Logo" id="google-logo" />
         구글로 시작하기
       </button>
 
       {/* 카카오 로그인 버튼 */}
-      <button className="login-button kakao-login" onClick={() => handleSocialLogin('kakao')}>
+      <button
+        className="login-button kakao-login"
+        onClick={() => handleSocialLogin('kakao')}
+      >
         <img src={KakaoIcon} alt="Kakao Logo" id="kakao-logo" />
         카카오로 시작하기
       </button>

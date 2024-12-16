a9ec9717aa3da832127eca93fde56c7dd3538068
Austin-Choi <austinsupra@gmail.com>
2024-12-16 21:18:09 +0900

    patch : 비정상 종료 보완 로직

--- src/components/CallControl.jsx
+++ src/components/CallControl.jsx
@@ -1,20 +1,26 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
 import EndCallIcon from '../assets/decline-button.svg';
+import DefaultProfile from '../assets/default-profile.svg';
 import './CallControl.css';
 
 const CallControl = ({ nickname, profileImage, onEndCall }) => {
-  const displayName = nickname;
+  const displayName = nickname || '비정상적 종료입니다.';
   const location = useLocation();
   const showEndCallButton = location.pathname !== '/call/end';
+  const resolvedProfileImage = profileImage || DefaultProfile;
 
   return (
     <div className="call-control">
       <div className="call-control-left">
         <img
-          src={profileImage}
+          src={resolvedProfileImage}
           alt="Profile"
           className="call-control-profile"
+          onError={e => {
+            e.target.onerror = null;
+            e.target.src = DefaultProfile;
+          }}
         />
         <span className="call-control-nickname">{nickname}</span>
       </div>

f0698c8c565f6abde84ddd9d3491e3220af30d0b
Austin-Choi <austinsupra@gmail.com>
2024-12-18 10:21:13 +0900

    patch : endcallscreen에서 비정상적 종료에 대한 ux적 개선

--- src/components/CallControl.jsx
+++ src/components/CallControl.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { useLocation } from 'react-router-dom';
 import EndCallIcon from '../assets/decline-button.svg';
 import DefaultProfile from '../assets/default-profile.svg';
 import './CallControl.css';
 
 const CallControl = ({ nickname, profileImage, onEndCall }) => {
-  const displayName = nickname || '비정상적 종료입니다.';
+  const displayName = useRef();
+  if (nickname === null || !nickname) {
+    displayName.current = '비정상적 종료!';
+  } else if (nickname === undefined) {
+    displayName.current = '익명';
+  } else {
+    displayName.current = nickname + '님 과의 통화 종료!';
+  }
   const location = useLocation();
   const showEndCallButton = location.pathname !== '/call/end';
   const resolvedProfileImage = profileImage || DefaultProfile;
@@ -22,7 +29,7 @@ const CallControl = ({ nickname, profileImage, onEndCall }) => {
             e.target.src = DefaultProfile;
           }}
         />
-        <span className="call-control-nickname">{displayName}</span>
+        <span className="call-control-nickname">{displayName.current}</span>
       </div>
       {showEndCallButton && (
         <button className="call-control-end-button" onClick={onEndCall}>

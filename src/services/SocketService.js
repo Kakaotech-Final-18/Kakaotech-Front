c0e932f531bed3092136f4e147d0ac7f6b9459a1
Austin-Choi <austinsupra@gmail.com>
2024-12-23 20:54:27 +0900

    patch : socket.io 연결 설정 세분화

--- src/services/SocketService.js
+++ src/services/SocketService.js
@@ -7,8 +7,13 @@ let socket;
  */
 export const initializeSocket = () => {
   if (!socket) {
-    const serverUrl = import.meta.env.VITE_SOCKET_URL;
-    socket = io(serverUrl);
+    const serverUrl = import.meta.env.VITE_SOCKET_URL || 'https://ptks.link';
+    socket = io(serverUrl, {
+      transports: ['polling', 'websocket'], // 폴링과 WebSocket 모두 사용 가능
+      reconnection: true, // 연결 끊김 시 재시도 활성화
+      reconnectionAttempts: 5, // 재시도 횟수 제한
+      reconnectionDelay: 1000, // 재시도 간격(ms)
+    });
 
     // 연결 끊김 확인
     socket.on('disconnect', reason => {
